2a6c66d6d5b6087a81a5ce55007b85b26fb0e7e2
Shervina-lim <58497215+Shervina-lim@users.noreply.github.com>
2019-12-06 15:20:01 +0800

    Add files via upload

---
+++ src/stopAll.cpp
@@ -0,0 +1,46 @@
+#include "ros/ros.h"
+#include "epos2/Velocity.h"
+#include "wrap.h"
+#include "std_msgs/Int32.h"
+int main(int argc, char **argv)
+{
+
+int lResult = MMC_FAILED;
+	unsigned int ulErrorCode = 0;
+	int node; 
+	// Set parameter for usb IO operation
+	SetDefaultParameters();
+	// open device
+	if((lResult = OpenDevice(&ulErrorCode))!=MMC_SUCCESS)
+	{
+		LogError("OpenDevice", lResult, ulErrorCode);
+		return lResult;
+	}
+	
+	SetEnableState(g_pKeyHandle, 1, &ulErrorCode);
+	ActivateProfileVelocityMode(g_pKeyHandle, 1, &ulErrorCode);
+
+	SetEnableState(g_pKeyHandle, 2 , &ulErrorCode);
+	ActivateProfileVelocityMode(g_pKeyHandle, 2, &ulErrorCode);
+	
+	// initialising ros stuffs
+	ros::init(argc, argv, "epos2");
+	ros::NodeHandle n;
+	ros::Publisher vel_pub = n.advertise<std_msgs::Int32>("vel",1000);
+	ROS_INFO("Ready to move.");	
+	
+	long int v1,v2;
+	v1=000;v2=000;
+	ROS_INFO("V1 : %ld", v1);
+	ROS_INFO("V2 : %ld", v2);
+	MoveWithVelocity(g_pKeyHandle, 1, (long)v1, &ulErrorCode);
+	MoveWithVelocity(g_pKeyHandle, 2, (long)v2, &ulErrorCode);
+	
+	//disable epos
+	SetDisableState(g_pKeyHandle, g_usNodeId, &ulErrorCode);
+	//close device
+	if((lResult = CloseDevice(&ulErrorCode))!=MMC_SUCCESS)
+	{
+		LogError("CloseDevice", lResult, ulErrorCode);
+		return lResult;
+	}
---
+++ src/testMotion.cpp
@@ -0,0 +1,54 @@
+#include "ros/ros.h"
+#include "epos2/Velocity.h"
+#include "wrap.h"
+#include "geometry_msgs/Vector3.h"
+int main(int argc, char **argv)
+{
+
+int lResult = MMC_FAILED;
+	unsigned int ulErrorCode = 0;
+	int node; 
+	// Set parameter for usb IO operation
+	SetDefaultParameters();
+	// open device
+	if((lResult = OpenDevice(&ulErrorCode))!=MMC_SUCCESS)
+	{
+		LogError("OpenDevice", lResult, ulErrorCode);
+		return lResult;
+	}
+	
+	//Activate both driver
+	SetEnableState(g_pKeyHandle, 1, &ulErrorCode);
+	ActivateProfileVelocityMode(g_pKeyHandle, 1, &ulErrorCode);
+	SetEnableState(g_pKeyHandle, 2, &ulErrorCode);
+	ActivateProfileVelocityMode(g_pKeyHandle, 2, &ulErrorCode);
+	
+	// initialising ros stuffs
+	ros::init(argc, argv, "epos2");
+	ros::NodeHandle n;
+	ros::Publisher vel_xy = n.advertise<geometry_msgs::Vector3>("vel",1000);
+	
+	long int Vx,Vy,w,v1,v2,v3;
+	ROS_INFO("Enter Vx"); cin >> Vx;
+	ROS_INFO("Enter Vy"); cin >> Vy;
+	if (Vx==0 && Vy ==0)
+		w=0;
+	else if (Vx<1000 || Vy<1000)
+		w=30;
+	else 
+		w=60;
+	// from https://www.maxongroup.fr/medias/sys_master/root/8806895255582/13-216-en.pdf
+		
+
+	v1 = Vy + w;
+	v2 = ((-sqrt(3)/2)*Vx) + (-0.5*Vy) + w;
+	v3 = (sqrt(3)/2)*Vx + (-0.5*Vy) + w;
+
+	
+	MoveWithVelocity(g_pKeyHandle, 1, (long)v1, &ulErrorCode);
+	MoveWithVelocity(g_pKeyHandle, 2, (long)v2, &ulErrorCode);
+	//MoveWithVelocity(g_pKeyHandle, 3, (long)v3, &ulErrorCode);
+
+	ros::spin();
+}
+
