0a41be059fc4c594a7cede920cd381e58eace1b2
Shervina-lim <58497215+Shervina-lim@users.noreply.github.com>
2019-12-07 00:36:48 +0800

    Update pub_cmdVel.cpp

--- src/pub_cmdVel.cpp
+++ src/pub_cmdVel.cpp
@@ -6,7 +6,7 @@
 int main(int argc, char **argv)
 {
 	// initialising ros stuffs
-	ros::init(argc, argv, "pub");
+	ros::init(argc, argv, "pub_vel");
 	ros::NodeHandle n;
 	ros::Publisher pub = n.advertise<geometry_msgs::Vector3>("/epos2/cmd_vel",1000);
 	
