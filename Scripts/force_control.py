a67fcb7875d46b9523f571bf38e9cbc11463b18d
Shervina-lim <58497215+Shervina-lim@users.noreply.github.com>
2019-12-11 17:43:09 +0800

    Update force_control.py

--- Scripts/force_control.py
+++ Scripts/force_control.py
@@ -6,9 +6,8 @@ from geometry_msgs.msg import Twist
 from geometry_msgs.msg import Vector3
 
 max_vel =100
-
+tmp = Twist()
 pub = rospy.Publisher('/epos2/cmd_vel', Twist, queue_size=10)
-
 
 def cb_once(data):
 
@@ -22,17 +21,36 @@ sub_once = rospy.Subscriber("/force",WrenchStamped,cb_once)
 def callback(data):
 
     v_x = (data.wrench.force.x-init_fx)*max_vel
-    #now = rospy-get_rostime()
-
-    tmp = Twist()
     tmp.linear = Vector3(v_x,0,0)
     tmp.angular = Vector3(0,0,0)
-    pub.publish(tmp)
+
 
 if __name__ == '__main__':
     rospy.init_node('ft_control', anonymous=True)
     rospy.Subscriber("/force", WrenchStamped, callback)
+    
+    rate = rospy.Rate(70) #10hz
+    while not rospy.is_shutdown():	
+    	pub.publish(tmp)	
+	rate.sleep()
 
     print "Start FT Control" 
     print "Initial fx =" + str(init_fx)
     rospy.spin()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
