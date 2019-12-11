9cf487f243903a4f14fdbd4f9ce7500e03b40ded
Shervina-lim <58497215+Shervina-lim@users.noreply.github.com>
2019-12-11 16:53:01 +0800

    Update pub_cmdVel.cpp

--- src/pub_cmdVel.cpp
+++ src/pub_cmdVel.cpp
@@ -1,21 +1,20 @@
 #include "ros/ros.h"
 #include "epos2/Velocity.h"
 #include "wrap.h"
-#include "geometry_msgs/Vector3.h"
+#include "geometry_msgs/Twist.h"
 
 int main(int argc, char **argv)
 {
 	// initialising ros stuffs
 	ros::init(argc, argv, "pub_vel");
 	ros::NodeHandle n;
-	ros::Publisher pub = n.advertise<geometry_msgs::Vector3>("/epos2/cmd_vel",1000);
+	ros::Publisher pub = n.advertise<geometry_msgs::Twist>("/epos2/cmd_vel",1000);
 	
 	long int Vx,Vy,w;
-	geometry_msgs::Vector3 tmp;	
-	ROS_INFO("Enter Vx"); cin >> Vx; tmp.x=Vx;
-	ROS_INFO("Enter Vy"); cin >> Vy; tmp.y=Vy;
-	ROS_INFO("Enter w"); cin >> w; tmp.z=w;
+	geometry_msgs::Twist tmp;	
+	ROS_INFO("Enter Vx"); cin >> Vx; tmp.linear.x = Vx;
+	ROS_INFO("Enter Vy"); cin >> Vy; tmp.linear.y = Vy;
+	ROS_INFO("Enter w"); cin >> w; tmp.angular.z = w;
 	pub.publish(tmp);
 	ros::spin();
 }
-
