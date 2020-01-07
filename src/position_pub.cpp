f75aee354f9eadbe757bf2e75e50e3e604fdad48
Shervina-lim <58497215+Shervina-lim@users.noreply.github.com>
2020-01-07 12:42:01 +0800

    Update position_pub.cpp

--- src/position_pub.cpp
+++ src/position_pub.cpp
@@ -20,19 +20,19 @@ int lResult = MMC_FAILED;
 	ActivateProfileVelocityMode(g_pKeyHandle, g_usNodeId, &ulErrorCode);
 	
 	// initialising ros stuffs
-	ros::init(argc, argv, "epos2");
+	ros::init(argc, argv, "encoder");
 	ros::NodeHandle n;
 	ros::Publisher pos_pub = n.advertise<std_msgs::Int32>("pos",1000);
 	ROS_INFO("Ready to move.");	
 	
-	long int targetPosition;
-	int isAbsolute =0;
-	ROS_INFO("Enter target position");
-	cin >> targetPosition;
+	//long int targetPosition;
+	//int isAbsolute =0;
+	//ROS_INFO("Enter target position");
+	//cin >> targetPosition;
 
-	ROS_INFO("request: target position=%ld", (long int)targetPosition);
+	//ROS_INFO("request: target position=%ld", (long int)targetPosition);
 	//MoveToPosition(g_pKeyHandle, g_usNodeId, (long)targetPosition, isAbsolute, &ulErrorCode);
-	MoveToPosition(g_pKeyHandle, g_usNodeId, 1000, 0, &ulErrorCode);
+	//MoveToPosition(g_pKeyHandle, g_usNodeId, 1000, 0, &ulErrorCode);
 	sleep(100);
 
 	short current;
@@ -42,12 +42,12 @@ int lResult = MMC_FAILED;
 	ROS_INFO("Current Position = %d", position_new);
 	
 	//disable epos
-	SetDisableState(g_pKeyHandle, g_usNodeId, &ulErrorCode);
+	//SetDisableState(g_pKeyHandle, g_usNodeId, &ulErrorCode);
 	//close device
-	if((lResult = CloseDevice(&ulErrorCode))!=MMC_SUCCESS)
-	{
-		LogError("CloseDevice", lResult, ulErrorCode);
-		return lResult;
-	}
-	return 0;
+	//if((lResult = CloseDevice(&ulErrorCode))!=MMC_SUCCESS)
+	//{
+	//	LogError("CloseDevice", lResult, ulErrorCode);
+	//	return lResult;
+	//}
+	//return 0;
 }
